bcf9b173bfcd67a192fc75e2854043cf53e0b97e
fuladorisabel <ingridfulador234@gmail.com>
2019-09-02 11:00:28 -0300

    sexta_versao

--- src/app/cidade/cidade-listar/cidade-listar.page.ts
+++ src/app/cidade/cidade-listar/cidade-listar.page.ts
@@ -1,27 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/internal/Observable';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { AngularFireDatabase } from '@angular/fire/database';
+import * as _ from 'lodash';
+
 import { Cidade } from '../entidade/cidade';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Router, NavigationExtras } from '@angular/router';
+
 @Component({
   selector: 'app-cidade-listar',
   templateUrl: './cidade-listar.page.html',
   styleUrls: ['./cidade-listar.page.scss'],
 })
-export class CidadeListarPage implements OnInit {
+export class ListarPage implements OnInit {
+
 
   listaCidade: Observable<Cidade[]>;
-  Cidade: Observable<{ key: string; }[]>;
+  listaFiltro: Cidade[];
+  filtro = {}; //regras ativas do filtro
+  cidades: any;
+  valor: string;
 
-  constructor(private fire: AngularFireDatabase) {
-    this.Cidade = this.fire.list<Cidade>('cidade').snapshotChanges().pipe(//busca
-      map(lista => lista.map(linha => ({
-        key: linha.payload.key, ...linha.payload.val()// seja formatado pela chave e pelo valor
-      })))
-    );//ira guardar esses contatos(lista), o fire tem os metodos necessarios para listar, e converter os dados para contato, configurando ela em linha(chave)
-
+  constructor(private fire: AngularFireDatabase, private rota: Router) {
+    this.listaCidade = this.fire.list<Cidade>('cidade').snapshotChanges().pipe(
+      map( lista => lista.map(linha => ({ key: linha.payload.key, ... linha.payload.val() })))
+    );
   }
+
   ngOnInit() {
+    this.listaCidade.subscribe(cidade => {
+        this.cidades = cidade;
+        this.listaFiltro = _.filter(this.cidades, _.conforms(this.filtro));
+    })
   }
-  //<ion-item *ngFor = "let contato of listaContatos | async"> serve para percorrer a lista de contatos
+
+  filtrar(){
+    this.filtro['nome'] = val => val.includes(this.valor);
+    this.listaFiltro = _.filter(this.cidades, _.conforms(this.filtro));
   }
+
+  excluir(key){
+    this.fire.list<Cidade>('cidade').remove(key);
+  }
+}
--- src/app/nome/listar/listar.page.ts
+++ src/app/nome/listar/listar.page.ts
@@ -3,6 +3,8 @@ import { Observable } from 'rxjs/internal/Observable';
 import { map } from 'rxjs/operators';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { Nome } from '../entidade/nome';
+import * as _ from 'lodash';
+import { Router, NavigationExtras } from '@angular/router';
 
 
 @Component({
@@ -10,16 +12,25 @@ import { Nome } from '../entidade/nome';
   templateUrl: './listar.page.html',
   styleUrls: ['./listar.page.scss'],
 })
-export class ListarPage implements OnInit{
+export class ListarPage implements OnInit {
+  [x: string]: any;
   listaNome: Observable<Nome[]>;
   Nome: Observable<{ key: string; }[]>;
-constructor(private fire: AngularFireDatabase) {
-this.Nome = this.fire.list<Nome>('nome').snapshotChanges().pipe(
-  map(lista => lista.map(linha => ({
-    key: linha.payload.key, ...linha.payload.val()
+  constructor(private fire: AngularFireDatabase) {
+    this.Nome = this.fire.list<Nome>('nome').snapshotChanges().pipe(
+      map(lista => lista.map(linha => ({
+        key: linha.payload.key, ...linha.payload.val()
       })))
-      );
+    );
   }
   ngOnInit() {
   }
- }
+filtrar(){
+  this.filtro['nome'] = val => val.includes(this.valor);
+  this.listaFiltro = _.filter(this.nome, _.conforms(this.filtro));
+}
+
+excluir(key){
+  this.fire.list<Nome>('nome').remove(key);
+}
+}
